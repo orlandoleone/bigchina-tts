00eb7e213f8b26610f07fee4dc5762a5ab315b04
orlandoleone <96437604+orlandoleone@users.noreply.github.com>
2024-09-11 23:03:05 +0200

    Update MPlayer.go

--- handlers/MPlayer.go
+++ handlers/MPlayer.go
@@ -8,6 +8,6 @@ type MPlayer struct {}
 
 func (MPlayer *MPlayer) Play(fileName string, flag int) error {
 	mplayer := exec.Command("mplayer", "-cache", "8092", "-", fileName)
-	flag == flag
+	_ = flag
 	return mplayer.Run()
 }
