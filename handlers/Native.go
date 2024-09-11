c3b66d732bd30916409e459800ad558b6bb19fce
orlandoleone <96437604+orlandoleone@users.noreply.github.com>
2024-09-11 14:57:30 +0200

    Update Native.go

--- handlers/Native.go
+++ handlers/Native.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"os"
 	"time"
+	"fmt"
 
 	"github.com/hajimehoshi/go-mp3"
 	"github.com/ebitengine/oto/v3"
@@ -13,10 +14,15 @@ type Native struct {
 
 func (n *Native) Play(fileName string) error {
 	// Read the mp3 file into memory
+
+	fmt.Print("a ")
 	file, err := os.Open(fileName)
+	fmt.Print("b ")
 	if err != nil {
 		return err
 	}
+
+	fmt.Print("c ")
 
 	// Decode file
 	decodedMp3, err := mp3.NewDecoder(file)
@@ -25,18 +31,27 @@ func (n *Native) Play(fileName string) error {
 		return err
 	}
  	*/
+	fmt.Print("d ")
 
 	op := &oto.NewContextOptions{}
 
+	fmt.Print("e ")
+
     	// Usually 44100 or 48000. Other values might cause distortions in Oto
     	op.SampleRate = 44100
+
+	fmt.Print("f ")
 
     	// Number of channels (aka locations) to play sounds from. Either 1 or 2.
     	// 1 is mono sound, and 2 is stereo (most speakers are stereo). 
     	op.ChannelCount = 2
 
+	fmt.Print("g ")
+
     	// Format of the source. go-mp3's format is signed 16bit integers.
     	op.Format = oto.FormatSignedInt16LE
+
+	fmt.Print("h ")
 
 	otoCtx, readyChan, err := oto.NewContext(op)
 
@@ -45,18 +60,30 @@ func (n *Native) Play(fileName string) error {
 		return err
 	}
  	*/
+
+	fmt.Print("i ")
 	
 	<-readyChan
 
+	fmt.Print("j ")
+
 	player := otoCtx.NewPlayer(decodedMp3)
 
+	fmt.Print("k ")
+
 	player.Play()
+
+	fmt.Print("l ")
 
 	for player.IsPlaying() {
 		time.Sleep(time.Second)
 	}
 
+	fmt.Print("m ")
+
 	err = player.Close()
+
+	fmt.Print("n ")
 	
 	return err
 }
